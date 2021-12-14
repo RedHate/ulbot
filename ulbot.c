a2b9b9f453af391a5d644a7b42ba631f08cf37ae
Ultros <redhate@ymail.com>
2021-12-14 17:45:59 -0500

    Update ulbot.c

--- ulbot.c
+++ ulbot.c
@@ -10,52 +10,52 @@
 #include <regex.h>
 
 int regcmp(const char *str, char *pattern){
-	
-    regex_t re;
-    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0)   return 0;
-    int ret = regexec(&re, str, (size_t) 0, NULL, 0);
-    regfree(&re);
-    if (ret != 0) return 0;
-    
-    return 1;
+
+	regex_t re;
+	if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0)   return 0;
+	int ret = regexec(&re, str, (size_t) 0, NULL, 0);
+	regfree(&re);
+	if (ret != 0) return 0;
+
+	return 1;
 }
 
 int main(int argc, char *argv[]){
 
-    if (argc < 4){
-        fprintf(stderr,"usage %s <ip/hostname> <port> <channel> <nick>\n", argv[0]);
-        exit(0);
-    }
+	if (argc < 4){
+		fprintf(stderr,"usage %s <ip/hostname> <port> <channel> <nick>\n", argv[0]);
+		exit(0);
+	}
 
 	//vars for sockets
-    struct sockaddr_in t_socket;
-    
-    //open the socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0){ 
+	struct sockaddr_in t_socket;
+
+	//open the socket
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0){ 
 		printf("ERROR opening socket");
 		exit(0);
 	}
-	
+
 	//get the server address
-    struct hostent *t_server = gethostbyname(argv[1]);
-    if (t_server == NULL){
-        printf("ERROR, no such host\n");
-        exit(0);
-    }
+	struct hostent *t_server = gethostbyname(argv[1]);
+	if (t_server == NULL){
+		printf("ERROR, no such host\n");
+		exit(0);
+	}
 
-    bzero((char *) &t_socket, sizeof(t_socket));
-    t_socket.sin_family = AF_INET;
-    bcopy((char *)t_server->h_addr,(char *)&t_socket.sin_addr.s_addr, t_server->h_length);
-    t_socket.sin_port = htons(atoi(argv[2]));
+	bzero((char *) &t_socket, sizeof(t_socket));
+	t_socket.sin_family = AF_INET;
+	bcopy((char *)t_server->h_addr,(char *)&t_socket.sin_addr.s_addr, t_server->h_length);
+	t_socket.sin_port = htons(atoi(argv[2]));
 
-    if (connect(sockfd,(struct sockaddr *) &t_socket,sizeof(t_socket)) < 0){
-	    printf("ERROR connecting\n");
-        exit(0);
+	if (connect(sockfd,(struct sockaddr *) &t_socket,sizeof(t_socket)) < 0){
+		printf("ERROR connecting\n");
+		exit(0);
 	}
 
 	unsigned char buf[512];
-	
+
 	sprintf(buf, "USER but guest moon sun\n\
 				  NICK %s\n\
 				  JOIN #%s\n", argv[4], argv[3]);
@@ -63,7 +63,7 @@ int main(int argc, char *argv[]){
 
 	int running = 1;
 	while(running){
-		
+
 		int n = read(sockfd,buf,sizeof(buf));
 		if(n){
 			if(regcmp(buf, "PRIVMSG .* :.fuck")){
@@ -100,14 +100,6 @@ int main(int argc, char *argv[]){
 				printf("%c", buf[i]);
 			}
 		}
-		/* INPUT NOT WORKING */
-		else{
-			int r = fread(&buf,sizeof(buf),512,stdin);
-			if(r){
-				write(sockfd,&buf,sizeof(buf));
-			}
-		}
-		
 	}
 
     close(sockfd);
